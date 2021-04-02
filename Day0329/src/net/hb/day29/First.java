9989f411e923da57a9b2fc1471e3983e980f997a
Kim <oo9110oo@naver.com>
2021-04-02 10:12:27 +0900

    04-02 금요일 10:12

---
+++ Day0329/src/net/hb/day29/First.java
@@ -0,0 +1,8 @@
+package net.hb.day29;
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("홍길동");
+		System.out.println("금요일");
+	}
+}
---
+++ Day0329/src/net/hb/day29/Two.java
@@ -0,0 +1,9 @@
+package net.hb.day29;
+
+public class Two {
+	public static void main(String[] args) {
+		System.out.println("Two");
+		System.out.println("Second");
+		System.out.println("Tea");
+	}
+}
